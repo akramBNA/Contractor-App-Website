bc1070b21a25a57eb80c1bf032686960b396a91c
Akram <akrambenaoun1993@gmail.com>
2025-04-16 12:28:14 +0100

    improve the who are we section, and adding new features to it!

--- src/components/who-are-we/who-are-we.component.ts
+++ src/components/who-are-we/who-are-we.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-who-are-we',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './who-are-we.component.html',
   styleUrl: './who-are-we.component.css',
 })
-export class WhoAreWeComponent {
+export class WhoAreWeComponent implements OnInit {
   loading = true;
 
-  constructor() {
-    setTimeout(() => {
+  ngOnInit() {
+    const img = new Image();
+    img.src = '/assets/soheba_logo.png';
+    img.onload = () => {
       this.loading = false;
-    }, 2000); // Simulates a 2s data fetch
+    };
   }
 }
