25b8e9fe3d7635d3c632a745f73795fb8e2a9da3
Akram <akrambenaoun1993@gmail.com>
2025-04-17 21:20:41 +0100

    create a statistics component

--- src/components/main-component/main-component.component.ts
+++ src/components/main-component/main-component.component.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { WhoAreWeComponent } from '../who-are-we/who-are-we.component';
 import { FooterComponent } from '../footer/footer.component';
-import { ContactUsComponent } from "../contact-us/contact-us.component";
+import { ContactUsComponent } from '../contact-us/contact-us.component';
+import { StatisticsComponent } from '../statistics/statistics.component';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // import AOS styles
+
 
 @Component({
   selector: 'app-main-component',
-  imports: [NavbarComponent, WhoAreWeComponent, FooterComponent, ContactUsComponent],
+  imports: [
+    NavbarComponent,
+    WhoAreWeComponent,
+    FooterComponent,
+    ContactUsComponent,
+    StatisticsComponent
+  ],
   templateUrl: './main-component.component.html',
-  styleUrl: './main-component.component.css'
+  styleUrl: './main-component.component.css',
 })
 export class MainComponentComponent {
-
+  ngOnInit() {
+    AOS.init({
+      duration: 1000, // Animation duration (ms)
+      once: true, // Trigger animation only once
+    });
+  }
 }
