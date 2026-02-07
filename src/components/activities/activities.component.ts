41f89ecb88fbf5c900d57b7f4eb4298cdc9015bb
Akram <akrambenaoun1993@gmail.com>
2026-02-07 23:38:09 +0100

    properly fit the Card's container in medium sized screens (tablet)

--- src/components/activities/activities.component.ts
+++ src/components/activities/activities.component.ts
@@ -1,4 +1,11 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,9 +15,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './activities.component.html',
   styleUrls: ['./activities.component.css'],
 })
-export class ActivitiesComponent implements OnInit {
+export class ActivitiesComponent implements OnInit, OnDestroy {
+  @ViewChild('carousel', { static: false })
+  carousel!: ElementRef<HTMLElement>;
+
   activeIndex = 0;
   isMobile = false;
+  autoScrollInterval: any;
 
   cards = [
     {
@@ -33,6 +44,11 @@ export class ActivitiesComponent implements OnInit {
 
   ngOnInit() {
     this.checkScreenSize();
+    this.startAutoScroll();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.autoScrollInterval);
   }
 
   @HostListener('window:resize')
@@ -44,22 +60,52 @@ export class ActivitiesComponent implements OnInit {
     this.isMobile = window.innerWidth < 1024;
   }
 
-  prevCard() {
-    this.activeIndex =
-      this.activeIndex === 0 ? this.cards.length - 1 : this.activeIndex - 1;
+  startAutoScroll() {
+    this.autoScrollInterval = setInterval(() => {
+      this.nextCard(true);
+    }, 5000);
   }
 
-  nextCard() {
+  resetAutoScroll() {
+    clearInterval(this.autoScrollInterval);
+    this.startAutoScroll();
+  }
+
+  prevCard(manual = false) {
+    this.activeIndex =
+      this.activeIndex === 0 ? this.cards.length - 1 : this.activeIndex - 1;
+
+    this.scrollToActive();
+    if (!manual) this.resetAutoScroll();
+  }
+
+  nextCard(manual = false) {
     this.activeIndex =
       this.activeIndex === this.cards.length - 1 ? 0 : this.activeIndex + 1;
+
+    this.scrollToActive();
+    if (!manual) this.resetAutoScroll();
+  }
+
+  scrollToActive() {
+    if (!this.carousel) return;
+
+    const container = this.carousel.nativeElement;
+    const cardWidth = container.clientWidth;
+
+    container.scrollTo({
+      left: this.activeIndex * cardWidth,
+      behavior: 'smooth',
+    });
   }
 
   onScroll(container: HTMLElement) {
-    const cardWidth = container.scrollWidth / this.cards.length;
+    const cardWidth = container.clientWidth;
     const index = Math.round(container.scrollLeft / cardWidth);
 
     if (index !== this.activeIndex) {
       this.activeIndex = index;
+      this.resetAutoScroll();
     }
   }
 }
